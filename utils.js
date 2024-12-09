556be8f6c286dc39248f95bc1b27aa784b544b52
René <reneskjodt333@gmail.com>
2024-12-09 16:23:41 +0100

    add UI templates for factions and info counts; refactor template retrieval function

--- utils.js
+++ utils.js
@@ -1,14 +1,15 @@
 // @ts-check
 
 /**
- * @param {string} templateId
- * @returns {HTMLElement}
+ * Retrieves a template from the DOM and clones its content.
+ *
+ * @param {string} templateId - The ID of the template element to retrieve.
+ * @returns {HTMLElement} A clone of the template's content.
+ * @throws {Error} If the element with the specified ID is not found.
  */
 export function templateCreate(templateId) {
-    const element = document.getElementById(templateId);
+    const element = /** @type {HTMLTemplateElement} */ document.getElementById(templateId);
     if (!element) throw new Error("could not find element " + templateId);
-    element.remove();
-    element.removeAttribute("id");
-    element.classList.remove("hidden");
-    return element;
+    if (!("content" in element)) throw new Error("element " + templateId + " does not have a content property");
+    return element.content.cloneNode(true).children[0];
 }
